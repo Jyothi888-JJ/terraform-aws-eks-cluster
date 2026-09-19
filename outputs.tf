e3b9f014a858c32339da5f913b891034c2a365c2
Jyothi888-JJ <jyothirmayi.jamisetti@gmail.com>
2026-09-19 09:56:00 +0530

    Add outputs for VPC and EKS resources

---
+++ outputs.tf
@@ -0,0 +1,19 @@
+output "vpc_id" {
+  description = "VPC ID"
+  value       = aws_vpc.main.id
+}
+
+output "eks_cluster_name" {
+  description = "EKS cluster name"
+  value       = aws_eks_cluster.main.name
+}
+
+output "eks_cluster_endpoint" {
+  description = "EKS cluster endpoint"
+  value       = aws_eks_cluster.main.endpoint
+}
+
+output "node_group_name" {
+  description = "EKS node group name"
+  value       = aws_eks_node_group.main.node_group_name
+}
